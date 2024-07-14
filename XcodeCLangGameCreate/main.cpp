c6362997e913f42a23df104d5a96ac7d95ffa9e1
DeveloperUchida <kouei28poiou@icloud.com>
2024-07-14 11:49:30 +0900

    DataUPadte 2024/07/14

--- XcodeCLangGameCreate/main.cpp
+++ XcodeCLangGameCreate/main.cpp
@@ -12,4 +12,12 @@ int main(){
     //乱数のシードを設定
     std::srand(static_cast<unsigned int>(std:: time(nullptr)));
     
+    //1から100までのランダムな数字を生成
+    int targetNumber = std::rand() % 100 +1;
+    int guess;
+    int attempts = 0;
+    
+    
+    std::cout << "数当てゲームへようこそ!" << std::endl;
+    
 }
