d77631da143b82297dbb75c95cb92f1649598e1a
DeveloperUchida <developeruchida@DeveloperUchidanoMac-mini.local>
2024-07-14 11:38:06 +0900

    FirstCommit 2024/07/14

---
+++ XcodeCLangGameCreate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XcodeCLangGameCreate
+//
+//  Created by DeveloperUchida on 2024/07/14.
+//
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main(){
+    //乱数のシードを設定
+    std::srand(static_cast<unsigned int>(std:: time(nullptr)));
+    
+}
